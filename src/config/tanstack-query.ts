cb415e4d623f31313b4fd75563f4f61763066aca
Mohamed Ashraf <mashraf@road9media.com>
2025-07-26 21:12:30 +0300

    refactor: tanstack query config

--- src/config/tanstack-query.ts
+++ src/config/tanstack-query.ts
@@ -6,16 +6,7 @@ let browserQueryClient: QueryClient | undefined = undefined;
 // Note: With SSR, we usually want to set some default staleTime
 // above 0 to avoid refetching immediately on the client
 const makeQueryClient = () =>
-	new QueryClient({
-		defaultOptions: {
-			queries: {
-				staleTime: 60 * 1000,
-				refetchOnMount: false,
-				refetchOnWindowFocus: false,
-				refetchOnReconnect: false,
-			},
-		},
-	});
+	new QueryClient({ defaultOptions: { queries: { staleTime: 60 * 1000 } } });
 
 const getQueryClient = () => {
 	// Server: always make a new query client
